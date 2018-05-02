e6fd7d88efc578d9ce6cfba1609b17ce4936f203
Masahito Kagawa <mkagawa@hotmail.com>
2018-05-02 16:59:35 -0700

    added license text

--- AMBEDConfig/Properties/AssemblyInfo.cs
+++ AMBEDConfig/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Masahito Kagawa (NW6UP)")]
 [assembly: AssemblyProduct("AMBEDConfig")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 by Masahito Kagawa NW6UP")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
